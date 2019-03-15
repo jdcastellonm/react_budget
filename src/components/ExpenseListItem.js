64f78e196ca1658d00c8c7b4b18244ddf43b89f3
Jose D. Castellon <jd.castellonm@gmail.com>
2019-03-14 21:38:16 -0400

    implement formatting for amount and date

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 // the spread can also be destructured like {description, amount, createdAt}
 // instead of using props.*
@@ -8,7 +10,7 @@ const ExpenseListItem = (props) => (
         <Link to={`/edit/${props.id}`}>
             <h3>{props.description}</h3>
         </Link>
-        <p>{props.amount} -- {props.createdAt}</p>
+        <p>{numeral(props.amount / 100).format('$0,0.00')} -- {moment(props.createdAt).format('MMMM Do YYYY')}</p>
     </div>
 )
 // export default connect()(ExpenseListItem); // access to state directly is not needed here, just the dispatch() (used when the remove button was here)
